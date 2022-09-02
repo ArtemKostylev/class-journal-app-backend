4309614a988fd373071d6df19eb93ae522c640ae
Artem Kostylev <shkri@194-67-67-84.cloudvps.regruhosting.ru>
2022-09-02 13:31:35 +0300

    start script correct path

---
+++ prisma/migrations/20220831071604_initial_migration/migration.sql
@@ -0,0 +1,8 @@
+-- AlterIndex
+ALTER INDEX "Course_parentId_unique" RENAME TO "Course.parentId_unique";
+
+-- AlterIndex
+ALTER INDEX "Replacement_entryId_unique" RENAME TO "Replacement.entryId_unique";
+
+-- AlterIndex
+ALTER INDEX "Teacher_userId_unique" RENAME TO "Teacher.userId_unique";
