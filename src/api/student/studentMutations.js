1675fb5a2a1bb25af59f97ae73fe36df036b43f7
19866015 <AAlKostylev@sberbank.ru>
2022-10-07 11:33:47 +0300

    Student update specId fix

--- src/api/student/studentMutations.js
+++ src/api/student/studentMutations.js
@@ -1,38 +1,39 @@
 const updateStudent = async (parent, args, context, info) => {
-  return await context.prisma.student.update({
-    where: {
-      id: args.data.id,
-    },
-    data: {
-      name: args.data.name,
-      surname: args.data.surname,
-      class: args.data.class,
-      program: args.data.program,
-    },
-  });
+    return await context.prisma.student.update({
+        where: {
+            id: args.data.id,
+        },
+        data: {
+            name: args.data.name,
+            surname: args.data.surname,
+            class: args.data.class,
+            program: args.data.program,
+            specializationId: args.data.specializationId
+        },
+    });
 };
 
 const deleteStudent = async (parent, args, context, info) => {
-  await context.prisma.student.delete({
-    where: {
-      id: args.id,
-    },
-  });
+    await context.prisma.student.delete({
+        where: {
+            id: args.id,
+        },
+    });
 };
 
 const createStudent = async (parent, args, context, info) => {
-  await context.prisma.student.create({
-    data: {
-      name: args.data.name,
-      surname: args.data.surname,
-      class: parseInt(args.data.class),
-      program: args.data.program,
-    },
-  });
+    await context.prisma.student.create({
+        data: {
+            name: args.data.name,
+            surname: args.data.surname,
+            class: parseInt(args.data.class),
+            program: args.data.program,
+        },
+    });
 };
 
 module.exports = {
-  updateStudent,
-  deleteStudent,
-  createStudent
+    updateStudent,
+    deleteStudent,
+    createStudent
 }
