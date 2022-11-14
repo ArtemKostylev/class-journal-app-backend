80d1ad2e7dd83046b8ab8ada74543603c328f23d
19866015 <AAlKostylev@sberbank.ru>
2022-11-14 17:25:40 +0300

    fixed consult bugs

--- src/api/consult/consultQueries.js
+++ src/api/consult/consultQueries.js
@@ -1,101 +1,91 @@
+const {getFreezeVersion} = require('../../queryUtils/getFreezeVersion');
 const fetchGroupConsults = async (parent, args, context) => {
-  const {userId} = context;
+    const {userId} = context;
 
-  const freezeVersion = await context.prisma.freezeVersion.findFirst({
-    where: {
-      year: args.year
-    }
-  }) || null
+    const freezeVersion = await getFreezeVersion(args.year, context.prisma);
 
-  let availableGroups = await context.prisma.teacher_Course_Student.findMany(
-      {
-        where: {
-          teacherId: args.teacherId,
-          courseId: args.courseId,
-          deleted: false,
-          freezeVersion: freezeVersion
-        },
-        select: {
-          subgroup: true,
-          student: {
-            select: {
-              class: true,
-              program: true,
+    let availableGroups = await context.prisma.teacher_Course_Student.findMany(
+        {
+            where: {
+                teacherId: args.teacherId,
+                courseId: args.courseId,
+                archived: false,
+                freezeVersionId: freezeVersion
             },
-          },
+            select: {
+                subgroup: true,
+                student: {
+                    select: {
+                        class: true,
+                        program: true,
+                    },
+                },
+            },
+        }
+    );
+
+    availableGroups = new Set(availableGroups.map((item) => `${item.student.class} ${item.student.program} ${item.subgroup || "..."}`));
+
+    const consultsAll = await context.prisma.groupConsult.findMany({
+        where: {
+            teacherId: args.teacherId,
+            courseId: args.courseId,
+            year: args.year,
         },
-      }
-  );
+    });
 
-  availableGroups = new Set(availableGroups.map(
-      (item) =>
-          `${item.student.class} ${item.student.program} ${item.subgroup || "..."
-          }`
-  ));
+    let consultsByGroups = new Map();
 
-  const consultsAll = await context.prisma.groupConsult.findMany({
-    where: {
-      teacherId: args.teacherId,
-      courseId: args.courseId,
-      year: args.year,
-    },
-  });
+    consultsAll.forEach((item) => {
+        const key = `${item.class} ${item.program} ${item.subgroup || "..."}`;
 
-  let consultsByGroups = new Map();
+        const value = {
+            id: item.id,
+            date: item.date,
+            hours: item.hours,
+        };
 
-  consultsAll.forEach((item) => {
-    const key = `${item.class} ${item.program} ${item.subgroup || "..."}`;
+        if (consultsByGroups.has(key)) {
+            consultsByGroups.set(key, [...consultsByGroups.get(key), value]);
+        } else {
+            consultsByGroups.set(key, [value]);
+        }
+    });
 
-    const value = {
-      id: item.id,
-      date: item.date,
-      hours: item.hours,
-    };
-
-    if (consultsByGroups.has(key)) {
-      consultsByGroups.set(key, [...consultsByGroups.get(key), value]);
-    } else {
-      consultsByGroups.set(key, [value]);
-    }
-  });
-
-  return Array.from(availableGroups).map((group) => {
-    if (consultsByGroups.has(group))
-      return {group, consults: consultsByGroups.get(group)};
-    return {group, consults: []};
-  });
+    return Array.from(availableGroups).map((group) => {
+        if (consultsByGroups.has(group))
+            return {group, consult: consultsByGroups.get(group)};
+        return {group, consult: []};
+    });
 };
 
 const fetchConsults = async (parent, args, context) => {
-  const {userId} = context;
+    const {userId} = context;
 
-  const freezeVersion = await context.prisma.freezeVersion.findFirst({
-    where: {
-      year: args.year
-    }
-  }) || null
+    const freezeVersion = await getFreezeVersion(args.year, context.prisma);
 
-  return await context.prisma.teacher_Course_Student.findMany({
-    where: {
-      teacherId: args.teacherId,
-      courseId: args.courseId,
-      freezeVersion: freezeVersion
-    },
-    include: {
-      consult: {
-        orderBy: {
-          date: "asc",
-        },
+    return await context.prisma.teacher_Course_Student.findMany({
         where: {
-          year: args.year,
+            teacherId: args.teacherId,
+            courseId: args.courseId,
+            freezeVersionId: freezeVersion,
+            archived: false
         },
-      },
-      student: true,
-    },
-  });
+        include: {
+            consult: {
+                orderBy: {
+                    date: "asc",
+                },
+                where: {
+                    year: args.year,
+                },
+            },
+            student: true,
+        },
+    });
 };
 
 module.exports = {
-  fetchConsults,
-  fetchGroupConsults
+    fetchConsults,
+    fetchGroupConsults
 }
